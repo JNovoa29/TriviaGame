c7b5b17e57a64c28df487da12a2803c8a718ccc6
Jordan Novoa <jordan.novoa15@gmail.com>
2018-02-23 22:00:33 -0800

    css, func, start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,10 @@ var startGameBTN
 $("#start").append(startGameBTN)
 
 //timer functionality tied to button click
-$(document).on("click", function () {
+
+$("#start").on("click", function () {
     var timeLeft = 30
+    $("#start").hide(0)
     var countdownTime = setInterval(function () {
         timeLeft--;
         $("#countdownTime").html("Time Remaining: " + timeLeft)
@@ -12,41 +14,43 @@ $(document).on("click", function () {
             clearInterval(countdownTime)
         // console.log("hi")
     }, 1000)
-
-})
-
-//trivia questions answers
-
-var question1
-var answers1 = [1991, 2000, 1993, 1995]
-var corrAns1 = 1995
-
-$(function() {
-    $("#ansrs1").html(answers1)
+    
+    // trivia questions and answers array
+    var trivia = [
+        {
+            question: "In what year did the original Sony Playstation release in North America?",
+            answers: ['1991', '2000', '1993', '1995'],
+            correctAnswer: '1995'
+        },
         
+        {
+            question: "Which goofy animal character is best known for only wearing pants?",
+            answers: ["PaRappa the Rapper", "Crash Bandicoot", "Spyro the Dragon", "Cloud"],
+            correctAnswer: "Crash Bandicoot"
+        }
+    ]
+    
+    //for loop to cycle through array of questions
+    
+    for (var i = 0; i < trivia.length; i++) {
+        var questObj = trivia[i]
+        var newQuestion = questObj.question
+        var questionAnswers = questObj.answers
+    }
+    $(".quest").html('<div id="activeQuestion" >'+newQuestion+'</div>') 
+    for(var j = 0; j < questionAnswers.length; j++) {
+        // trivia[i].answers
+        $('.ansChoices').append('<input type="radio">'+questionAnswers[j]+'</input><br>')
+    }
 })
-
-
-
-// $("#ans").click(function() {    
-    //trivia questions and answers array
-    // var trivia = [
-    //     {
-    //         question1: "In what year did the original Sony Playstation release in North America?",
-    //         answers: [1991, 2000, 1993, 1995],
-    //         correctAnswer: 1995
     
-    //     },
+    console.log(newQuestion)
+    console.log(questionAnswers)
     
-    //     {
-    //         question2: "Which goofy animal character is best known for only wearing pants?",
-    //         answers: ["PaRappa the Rapper", "Crash Bandicoot", "Spyro the Dragon", "Cloud"],
-    //         correctAnswer: "Crash Bandicoot"
-    //     }
     
-    // ]
-//   });
-
+    
+    
+    
 
 
 
